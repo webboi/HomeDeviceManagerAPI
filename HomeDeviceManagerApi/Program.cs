5adc03111b59f24de594fd30c7cf7363e41c8a64
webboi <isaacwebbo189@gmail.com>
2023-12-15 14:52:02 +0300

    allowed corsfile from all origins...

--- HomeDeviceManagerApi/Program.cs
+++ HomeDeviceManagerApi/Program.cs
@@ -17,7 +17,7 @@ options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection"))
 var app = builder.Build();
 app.UseCors(options =>
 {
-    options.WithOrigins("http://localhost:54364");
+    options.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
 });
 
 // Configure the HTTP request pipeline.
